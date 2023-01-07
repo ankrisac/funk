e18f51fa0a787104af4c3c97709248cf9c520066
ankrisac <ankrisac@gmail.com>
2023-01-07 22:04:56 +0000

    added more methods to option

--- include/option.hpp
+++ include/option.hpp
@@ -19,10 +19,21 @@ public:
     return out << "None";
   }
 
-  // Apply function to value of Some
+  inline bool is_some() {
+    return has_value;
+  }
+  inline bool is_none() {
+    return !has_value;
+  }
+
   template<typename Fn> 
   Option<Invoke<Fn, T>> map(Fn fn) {
     return has_value ? Some(std::invoke(fn, value)) : None;
+  }
+  template<typename Pred>
+    requires std::same_as<Invoke<Pred, T>, bool> 
+  Option<T> filter(Pred pred) {
+    return (has_value && pred(value)) ? Some(value) : None;
   }
 
   template<typename Fn> 
@@ -31,6 +42,21 @@ public:
   }
   Option and_take(Option<T> value) {
     return has_value ? value : None;
+  }
+
+  template<typename Fn> 
+    requires CanInvoke<Fn, T> 
+  Option inspect_some(Fn fn) {
+    if(has_value) {
+      std::invoke(fn, value);
+    }
+  }
+  template<typename Fn> 
+    requires CanInvoke<Fn> 
+  Option inspect_none() {
+    if(has_value) {
+      std::invoke()
+    }
   }
 };
 
